896d86db62a224ff64d6b34a033f8d2462cfa154
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2026-04-19 14:53:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 import pygame
 from utils.constants import (SCREEN_WIDTH, SCREEN_HEIGHT, FPS, TITLE,
                               SCENE_MENU, SCENE_PROLOGUE, SCENE_LEVEL1,
-                              SCENE_LEVEL2, SCENE_HANDOFF)
+                              SCENE_LEVEL2, SCENE_HANDOFF, SCENE_GAMEOVER)
 from utils.game_state  import GameState
 from scenes.main_menu  import MainMenuScene
 from scenes.prologue   import PrologueScene
 from scenes.level1     import Level1Scene
 from scenes.handoff    import HandoffScene
 from scenes.level2     import Level2Scene
+from scenes.gameover   import GameOverScene
 
 
 def main():
@@ -26,6 +27,7 @@ def main():
         SCENE_LEVEL1:   Level1Scene,
         SCENE_HANDOFF:  HandoffScene,
         SCENE_LEVEL2:   Level2Scene,
+        SCENE_GAMEOVER: GameOverScene,
     }
 
     current_scene = MainMenuScene(screen, state)
